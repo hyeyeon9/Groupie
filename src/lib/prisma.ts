9d579b69ac32dee31238a78f3117964c3f0d9ea3
구혜연 <guhaa03@daum.net>
2025-05-22 22:44:06 +0900

    feat(prisma): 기본 스키마 초기화

---
+++ src/lib/prisma.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  // 배포환경이면
+  globalForPrisma.prisma = prisma;
+}
