9d476ed7d93125a31f291b2d997150c14401561e
구혜연 <guhaa03@daum.net>
2025-05-22 22:48:44 +0900

    feat(study): 스터디 게시글 리스트 페이지 구현

---
+++ src/app/(auth)/study/page.tsx
@@ -0,0 +1,11 @@
+import StudyList from "@/components/StudyList";
+import Link from "next/link";
+
+export default function StudyHomePage() {
+  return (
+    <div className="min-h-screen w-full p-10">
+      <Link href="/newStudy">스터디 모집</Link>
+      <StudyList />
+    </div>
+  );
+}
