72a9b041383b06d77084f03f358551cd40ed20b9
구혜연 <guhaa03@daum.net>
2025-05-28 18:08:59 +0900

    feat(ui): 로그인/회원가입 모달 UI 컴포넌트 구현

---
+++ src/components/LoginSignupModal.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { useModalStore } from "@/store/modalStore";
+import { Dialog, DialogPanel } from "@headlessui/react";
+import LoginForm from "./forms/LoginForm";
+import SignupForm from "./forms/SignupForm";
+
+export default function LoginSignupModal() {
+  const { openModal, close } = useModalStore();
+
+  return (
+    <Dialog open={!!openModal} onClose={close} className="relative z-50">
+      <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+      <div className="fixed inset-0 flex items-center justify-center p-4">
+        <DialogPanel className="w-full max-w-md rounded-lg bg-white p-6">
+          {openModal === "login" ? <LoginForm /> : <SignupForm />}
+        </DialogPanel>
+      </div>
+    </Dialog>
+  );
+}
